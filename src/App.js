aec54bedcafcd53e275b54609ed66409497d91fe
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-08-25 16:29:27 +1000

    add submiting feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./App.css";
+
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
   
   // Add the handlesubmit code here
+  function handleSubmit(e) {
+    e.preventDefault();
   
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+  
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+    } else {
+      alert("Invalid Task");
+    }
+    setTodo("");
+  }
   
   // Add the deleteToDo code here
 
@@ -19,10 +35,16 @@ const App = () => {
   return(
     <div className ="App">
       <h1>Todo List</h1>
-      <form>
-      <input type ="text" align ="right" />
-      <button type ="submit">Add Todo</button>
+      <form onSubmit={handleSubmit}>
+        <input type ="text"
+          align ="right"
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Add your new task"
+          value={todo}  
+        />
+        <button type ="submit">Add Todo</button>
       </form>
+      {todos.map((todo) => <div>{todo.text}</div>)}
     </div>
   );
 };
